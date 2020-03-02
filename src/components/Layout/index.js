66763bf709a576fb7d099566ca40c458e43b6a77
Raims Cabrera <raimscabrera@gmail.com>
2020-03-02 18:29:24 +0800

    Runs prettier

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -5,12 +5,12 @@
  * See: https://www.gatsbyjs.org/docs/static-query/
  */
 
-import React from "react";
-import PropTypes from "prop-types";
-import { StaticQuery, graphql } from "gatsby";
+import React from 'react'
+import PropTypes from 'prop-types'
+import { StaticQuery, graphql } from 'gatsby'
 
-import Header from "../header";
-import "./Layout.css";
+import Header from '../header'
+import './Layout.css'
 
 const Layout = ({ children }) => (
   <StaticQuery
@@ -36,10 +36,10 @@ const Layout = ({ children }) => (
       </>
     )}
   />
-);
+)
 
 Layout.propTypes = {
-  children: PropTypes.node.isRequired
-};
+  children: PropTypes.node.isRequired,
+}
 
-export default Layout;
+export default Layout
